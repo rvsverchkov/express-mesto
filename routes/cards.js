2919bec6c4fd81daa486a0f799decfdaaf5445e4
rvsverchkov <s10z40@yandex.ru>
2020-10-17 12:17:17 +0300

    Project 12 - fix errors

--- routes/cards.js
+++ routes/cards.js
@@ -1,13 +1,17 @@
 const router = require('express').Router();
-const readFile = require('../utils/read');
 const path = require('path');
+const readFile = require('../utils/read');
+
 const jsonDataPath = path.join(__dirname, '..', 'data', 'cards.json');
 
 router.get('/cards', (req, res) => {
   readFile(jsonDataPath)
-    .then(data => {
+    .then((data) => {
       res.send(data);
     })
-})
+    .catch((err) => {
+      res.status(500).send({ error: `Ошибка на сервере: ${err.message}` });
+    });
+});
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,28 +1,35 @@
 const router = require('express').Router();
-const readFile = require('../utils/read');
 const path = require('path');
+const readFile = require('../utils/read');
+
 const jsonDataPath = path.join(__dirname, '..', 'data', 'users.json');
 
 router.get('/users', (req, res) => {
   readFile(jsonDataPath)
-    .then(data => {
+    .then((data) => {
       res.send(data);
     })
-})
+    .catch((err) => {
+      res.status(500).send({ error: `Ошибка на сервере: ${err.message}` });
+    });
+});
 
 router.get('/users/:id', (req, res) => {
   const { id } = req.params;
   readFile(jsonDataPath)
-    .then(data => {
-      const user = data.users.find(currentUser => currentUser._id === id);
+    .then((data) => {
+      const user = data.users.find((currentUser) => currentUser._id === id);
       return user;
     })
-    .then(user => {
+    .then((user) => {
       if (!user) {
-        return res.status(404).send({ message: `Нет пользователя с таким id, как ${id}`});
+        return res.status(404).send({ message: `Нет пользователя с таким id, как ${id}` });
       }
-      res.send(user);
+      return res.send(user);
     })
-})
+    .catch((err) => {
+      res.status(500).send({ error: `Ошибка на сервере: ${err.message}` });
+    });
+});
 
 module.exports = router;
