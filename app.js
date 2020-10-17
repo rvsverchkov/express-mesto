f8e44b8d6d128afdced63c78d4b6e495f8dbd152
rvsverchkov <s10z40@yandex.ru>
2020-10-17 12:34:18 +0300

    Project 12 - fix errors with index.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const app = express();
 const PORT = 3000;
 const path = require('path');
@@ -9,9 +10,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', usersRoutes);
 app.use('/', cardsRoutes);
 app.use('*', (req, res) => {
-  res.status(404).send({ message: 'Запрашиваемый ресурс не найден'});
+  res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
 });
 
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
-})
+});
