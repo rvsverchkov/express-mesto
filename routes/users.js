25fccd41966a50fa18c037e909e82eab0be5cee8
rvsverchkov <s10z40@yandex.ru>
2020-11-28 13:37:33 +0300

    Project 15 - Валидация запросов

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 /* eslint-disable linebreak-style */
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const auth = require('../middlewares/auth.js');
 const {
   getUsers, createUser, updateProfile, updateAvatar, login, getUserInfo,
@@ -7,8 +8,21 @@ const {
 
 router.get('/users', auth, getUsers);
 router.get('/users/me', auth, getUserInfo);
-router.post('/signup', createUser);
-router.post('/signin', login);
+router.post('/signup', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string(),
+  }),
+}), createUser);
+router.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), login);
 router.patch('/users/me', auth, updateProfile);
 router.patch('/users/me/avatar', auth, updateAvatar);
 
