89db04c90fee96c3636d1d9e5f21ae0783fdea55
rvsverchkov <s10z40@yandex.ru>
2020-12-01 23:04:32 +0300

    Project 15 - fix password hash

--- models/user.js
+++ models/user.js
@@ -44,11 +44,18 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: true,
+    select: false,
   },
 });
 
+userSchema.methods.toJSON = function () {
+  const obj = this.toObject();
+  delete obj.password;
+  return obj;
+};
+
 userSchema.statics.findUserByCredentials = function (email, password) {
-  return this.findOne({ email })
+  return this.findOne({ email }).select('+password')
     .then((user) => {
       if (!user) {
         return Promise.reject(new Error('Неправильные почта или пароль'));
